800a3b8dd1261ec2ca98869563176d6b7e68dd37
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2026-06-04 20:31:31 -0300

    feat: aht10 driver api template

--- module_drivers/aht10/drivers/src/aht10.c
+++ module_drivers/aht10/drivers/src/aht10.c
@@ -4,9 +4,99 @@
 
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/sensor.h>
+#include <zephyr/drivers/i2c.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
 #include "aht10.h"
 
 LOG_MODULE_REGISTER(aht10, CONFIG_AHT10_LOG_LEVEL);
+
+// AHT10 driver configuration structure
+struct aht10_config {
+  struct i2c_dt_spec i2c;
+};
+
+// AHT10 driver data structure
+struct aht10_data {
+  float temperature;
+  float humidity;
+};
+
+// Internal implementations
+static int aht10_drv_read_raw(const struct device *dev) {
+  const struct aht10_config *config = dev->config;
+  struct aht10_data *data = dev->data;
+  // Read raw data from the AHT10 sensor using I2C
+  // This is a placeholder, actual I2C read commands should be implemented here
+  LOG_DBG("Reading raw data from AHT10 sensor");
+  // Simulate reading temperature and humidity values
+  data->temperature = 25.0; // Placeholder value
+  data->humidity = 50.0;    // Placeholder value
+  return 0;
+}
+
+
+static int aht10_drv_init(const struct device *dev)
+{
+  const struct aht10_config *config = dev->config;
+  struct aht10_data *data = dev->data;
+
+  if (!device_is_ready(config->i2c.bus)) {
+    LOG_ERR("I2C bus is not ready");
+    return -ENODEV;
+  }
+
+  // Initialize the AHT10 sensor (send initialization commands)
+  // This is a placeholder, actual initialization commands should be sent here
+  LOG_DBG("Initializing AHT10 sensor");
+
+  return 0;
+}
+
+static int aht10_drv_read_temperature(const struct device *dev, float *temperature)
+{
+  const struct aht10_config *config = dev->config;
+  struct aht10_data *data = dev->data;
+
+  aht10_drv_read_raw(dev);
+  // Read temperature data from the AHT10 sensor
+  // This is a placeholder, actual I2C read commands should be implemented here
+  LOG_DBG("Reading temperature from AHT10 sensor");
+
+  *temperature = data->temperature; // Return the stored temperature value
+  return 0;
+}
+
+static int aht10_drv_read_humidity(const struct device *dev, float *humidity)
+{
+  const struct aht10_config *config = dev->config;
+  struct aht10_data *data = dev->data;
+
+  aht10_drv_read_raw(dev);
+  // Read humidity data from the AHT10 sensor
+  // This is a placeholder, actual I2C read commands should be implemented here
+  LOG_DBG("Reading humidity from AHT10 sensor");
+
+  *humidity = data->humidity; // Return the stored humidity value
+  return 0;
+}
+
+// Define the AHT10 driver API structure
+static const struct aht10_driver_api aht10_api = {
+  .init = aht10_drv_init,
+  .read_temperature = aht10_drv_read_temperature,
+  .read_humidity = aht10_drv_read_humidity,
+};
+
+#define AHT10_INIT(n) \
+    static const struct aht10_config aht10_config_##n = { \
+        .i2c = I2C_DT_SPEC_INST_GET(n), \
+    }; \
+    static struct aht10_data aht10_data_##n; \
+    DEVICE_DT_INST_DEFINE(n, aht10_drv_init, NULL, \
+                          &aht10_data_##n, &aht10_config_##n, \
+                          POST_KERNEL, CONFIG_AHT10_INIT_PRIORITY, \
+                          &aht10_api);
+
+DT_INST_FOREACH_STATUS_OKAY(AHT10_INIT)
--- module_drivers/aht10/include/aht10.h
+++ module_drivers/aht10/include/aht10.h
@@ -1,4 +1,65 @@
 #ifndef _AHT10_H_
 #define _AHT10_H_
 
+#include <zephyr/device.h>
+#include <zephyr/toolchain.h>
+
+__subsystem struct aht10_driver_api {
+	/**
+	 * @brief Configure the AHT10 sensor.
+	 *
+	 * @param dev AHT10 device instance.
+	 *
+	 * @retval 0 if successful.
+	 * @retval -errno Other negative errno code on failure.
+	 */
+	int (*init)(const struct device *dev);
+  /**
+   * @brief Read the temperature from the AHT10 sensor.
+   *
+   * @param dev AHT10 device instance.
+   * @param temperature Pointer to store the read temperature.
+   *
+   * @retval 0 if successful.
+   * @retval -errno Other negative errno code on failure.
+   */
+  int (*read_temperature)(const struct device *dev, float *temperature);
+  /**
+   * @brief Read the humidity from the AHT10 sensor.
+   *
+   * @param dev AHT10 device instance.
+   * @param humidity Pointer to store the read humidity.
+   *
+   * @retval 0 if successful.
+   * @retval -errno Other negative errno code on failure.
+   */
+  int (*read_humidity)(const struct device *dev, float *humidity);
+};
+
+__syscall int aht10_init(const struct device *dev);
+
+static inline int z_impl_aht10_init(const struct device *dev)
+{
+	__ASSERT_NO_MSG(DEVICE_API_IS(aht10, dev));  
+	return DEVICE_API_GET(aht10, dev)->init(dev);
+}
+
+__syscall int aht10_read_temperature(const struct device *dev, float *temperature);
+
+static inline int z_impl_aht10_read_temperature(const struct device *dev, float *temperature)
+{
+  __ASSERT_NO_MSG(DEVICE_API_IS(aht10, dev));  
+  return DEVICE_API_GET(aht10, dev)->read_temperature(dev, temperature);
+}
+
+__syscall int aht10_read_humidity(const struct device *dev, float *humidity);
+
+static inline int z_impl_aht10_read_humidity(const struct device *dev, float *humidity)
+{
+  __ASSERT_NO_MSG(DEVICE_API_IS(aht10, dev));  
+  return DEVICE_API_GET(aht10, dev)->read_humidity(dev, humidity);
+}
+
+#include <syscalls/aht10.h>
+
 #endif
