1b1643634df8ba22c1e660b91495cac7f05fc53e
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2026-06-04 16:19:08 -0300

    fix: include path for driver header

--- module_drivers/aht10/drivers/src/aht10.c
+++ module_drivers/aht10/drivers/src/aht10.c
@@ -1,1 +1,12 @@
+#define DT_DRV_COMPAT aht10
+
+#include <zephyr/device.h>
+
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
 #include "aht10.h"
+
+LOG_MODULE_REGISTER(aht10, CONFIG_AHT10_LOG_LEVEL);
