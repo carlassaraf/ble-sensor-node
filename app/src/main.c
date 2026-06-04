43d22971631304eb38dd46b8f896795a3745daed
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2026-06-04 20:32:40 -0300

    feat: sensor basic test

--- app/src/main.c
+++ app/src/main.c
@@ -1,9 +1,37 @@
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <aht10.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 int main(void)
 {
+  const struct device *aht10_dev = DEVICE_DT_GET(DT_NODELABEL(aht10));
+  if(!device_is_ready(aht10_dev)) {
+    LOG_ERR("AHT10 device is not ready");
+    return -1;
+  }
+
+  int ret = aht10_init(aht10_dev);
+  if (ret) {
+    LOG_ERR("Failed to initialize AHT10 device (err %d)", ret);
+    return -1;
+  }
+
   while(1) {
-    
+    float temp, humidity;
+    ret = aht10_read_temperature(aht10_dev, &temp);
+    if (ret) {
+      LOG_ERR("Failed to read temperature from AHT10 device (err %d)", ret);
+      return -1;
+    }
+    ret = aht10_read_humidity(aht10_dev, &humidity);
+    if (ret) {
+      LOG_ERR("Failed to read humidity from AHT10 device (err %d)", ret);
+      return -1;
+    }
+    LOG_INF("Temperature: %.2f °C, Humidity: %.2f %%", temp, humidity);
+    k_msleep(500);
   }
   return 0;
 }
