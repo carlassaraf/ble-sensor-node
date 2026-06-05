f43d70f2e785ac5c99bd079e19857cf80d802236
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2026-06-04 21:14:52 -0300

    refactor: proper compatible string

--- module_drivers/aht10/drivers/src/aht10.c
+++ module_drivers/aht10/drivers/src/aht10.c
@@ -1,9 +1,8 @@
-#define DT_DRV_COMPAT aht10
+#define DT_DRV_COMPAT asair_aht10
 
 #include <zephyr/device.h>
 
 #include <zephyr/devicetree.h>
-#include <zephyr/drivers/sensor.h>
 #include <zephyr/drivers/i2c.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
